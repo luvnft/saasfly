64cc9b3cc2329b74ea3a10b080caec1f88ab9352
The Wizard of Hahz <info@luvnft.com>
2024-10-24 12:42:26 -0400

    up

--- apps/nextjs/src/config/ui/marketing.ts
+++ apps/nextjs/src/config/ui/marketing.ts
@@ -24,10 +24,6 @@ export const getMarketingConfig = async ({
         title: dict.marketing.main_nav_blog,
         href: `/blog`,
       },
-      {
-        title: dict.marketing.main_nav_documentation,
-        href: `/docs`,
-      },
     ],
   };
 };
